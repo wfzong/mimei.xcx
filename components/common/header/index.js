9f7193fe935f7332ad31ec9a5499d1db658e49cd
fuzong.wang <fuzong.wang@ikang.com>
2018-08-29 18:49:08 +0800

    update header

--- components/common/header/index.js
+++ components/common/header/index.js
@@ -12,13 +12,18 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    close: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    menuTrigger: function() {
+      this.setData({
+        close: !this.data.close
+      })
+    }
 
   }
 })
