40dd1a562903fca8424ac94568c8c469fd4bb55c
王福宗 <wfz123@126.com>
2023-08-07 22:02:46 +0800

    民生促销页-初稿

---
+++ pages/ms-promote/index.js
@@ -0,0 +1,88 @@
+// pages/ms-promote/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    swiperIndex: 1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+  swiperChange(event) {
+    console.log(event)
+    this.setData({
+      swiperIndex: event.detail.current
+    })
+  },
+  changeAdd() {
+    this.setData({
+      swiperIndex: this.data.swiperIndex + 1
+    })
+  },
+  changeDee() {
+    this.setData({
+      swiperIndex: this.data.swiperIndex - 1
+    })
+  }
+})
